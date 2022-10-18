20576684bab4d9cf7725bd3344103704a6d4eaf4
pgiardiniere <pgiardiniere@gmail.com>
2022-10-17 23:46:43 -0400

    working sample + instructions in readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # drawr
+
+Todo: 
+  Delete the samples that don't work
+  Delete the conda envs that don't work
---
+++ sample.py
@@ -0,0 +1,17 @@
+# https://huggingface.co/CompVis/stable-diffusion-v1-4
+
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+model_id = "CompVis/stable-diffusion-v1-4"
+device = "cuda"
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, revision="fp16", use_auth_token=True)
+pipe = pipe.to(device)
+
+prompt = "a photo of an astronaut riding a horse on mars"
+with autocast("cuda"):
+    image = pipe(prompt, guidance_scale=7.5).images[0]  
+    
+image.save("astronaut_rides_horse.png")
